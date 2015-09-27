c208e1589af3ee01fd09ad0f44950eaeee5ccaf7
Iván idalgo <alexis-hp@hotmail.com>
2015-09-27 01:10:01 -0500

    Modificacion tarea 2:  se agregaron comentarios  para explicar el codigo

--- Tareas/tarea2.py
+++ Tareas/tarea2.py
@@ -6,12 +6,12 @@
 #Calcular el tiempo necesario, en segundos, para que la ambulancia llegue a su destino. La formula a utilizar es: velocidad = distancia / tiempo.
 
 
-velocidad  =  120
+velocidad  =  120                   
 distancia = 60
 
-tiempo_h = distancia / velocidad
-tiempo_m = tiempo_h * 60 
-tiempo_s = tiempo_m * 60 
+tiempo_h = distancia / velocidad    #calcula  el  tiempo en horas
+tiempo_m = tiempo_h * 60            #calcula  el  tiempo en minutos
+tiempo_s = tiempo_m * 60            #calcula  el  tiempo en segundos
 
 print ("El tiempo necesario para que la ambulancia llegue a su destino es : " + str(tiempo_s) + "s") 
  
