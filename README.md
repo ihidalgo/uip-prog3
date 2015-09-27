d908b884812e8457f766ebf780e4fd0d790dce9f
Iván idalgo <alexis-hp@hotmail.com>
2015-09-27 01:03:09 -0500

    Tarea 2 completada exitosamente

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-Programación de Computadoras III - UIP Autor: Iván Hidalgo Fecha: 21/09/2015 Versión: 1.0
+
+
+======================================
+Programación de Computadoras III - UIP
+======================================
+
+:Autor: Iván Hidalgo
+:Fecha: 21/09/2015
+:Versión: 1.0
+
+Mi repositorio de trabajo para la clase de Programacion de Computadoras III, Universidad Interamericana de Panama.
+—-------
+Licencia
+—-------
+MIT
---
+++ Tareas/tarea2.py
@@ -0,0 +1,18 @@
+#TAREA2
+#Autor: Ivan Hidalgo
+#
+#Instrucciones:Crear un programa en Python que resuelva el siguiente problema de fisica: 
+#Una ambulancia se mueve con una velocidad de 120 km/h y necesita recorrer un tramo recto de 60km.
+#Calcular el tiempo necesario, en segundos, para que la ambulancia llegue a su destino. La formula a utilizar es: velocidad = distancia / tiempo.
+
+
+velocidad  =  120
+distancia = 60
+
+tiempo_h = distancia / velocidad
+tiempo_m = tiempo_h * 60 
+tiempo_s = tiempo_m * 60 
+
+print ("El tiempo necesario para que la ambulancia llegue a su destino es : " + str(tiempo_s) + "s") 
+ 
+
