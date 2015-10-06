5caa6a846a734e8abc2de639c2246c4b3c525cf6
ihidalgo <alexis-hp@hotmail.com>
2015-10-06 17:13:25 -0500

    Tarea 3 modificada y completada con exito

--- Tareas/Tarea3.py
+++ Tareas/Tarea3.py
@@ -4,17 +4,16 @@
 #Escriba un programa en Python donde el usuario introduce un numero n y el programa 
 #imprime los primeros n numeros triangulares, junto con su indice. Los numeros triangulares se originan
 #de la suma de los numeros naturales desde 1 hasta n.
-#Ecuacion  utilizada S n i=1  i=n*(n+1)/2
+#Ecuacion  utilizada Sigma n i=1  i=n*(n+1)/2
 
 
-numero=int(input("ingrese un numero: "))
-resultado=[]
+numero=int(input("Ingrese un numero: "))
+resultado={}
 
 for i in range(1,numero+1):
    resultado[i]=(i*(i+1)/2)
-
 for i in resultado:
-print (i, resultado[i])
+   print (i, resultado[i])
 
  
 
